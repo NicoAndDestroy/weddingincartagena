93aea669d4ce2d69721bd655ea60c1a61fea860b
Hansel M. Rojas <co.hanselmorales@gmail.com>
2024-07-28 10:30:35 -0500

    feat (logo): setup y configuracion con preferencia de color dark o light

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,37 +15,39 @@ export default {
       gray: colors.gray,
       indigo: colors.indigo,
       neutral: colors.neutral,  // Used mainly for text color
+      primary: {
+        500: "#d7b286", // Main color
+        600: "#c19a6e",
+        700: "#a8835a",
+        800: "#8f6c46",
+        900: "#765533",
+        950: "#5d3e20"  // Darkest shade
+      },
       yellow: {
-        50: "#faf5e4", // Lightest shade
+        50: "#faf5e4",  // Lightest shade
         100: "#f5ebc8",
         200: "#f0e1ac",
         300: "#ebd790",
         400: "#e6cd74",
-        500: "#d4af37", // Main color
-        600: "#b9972f",
-        700: "#9e7f27",
-        800: "#83671f",
-        950: "#68500f", // Darkest shade
+        500: "#C3AC4F ", // Main color
+        600: "#c19a6e",
+        700: "#a8835a",
+        800: "#8f6c46",
+        900: "#765533",
+        950: "#5d3e20"  // Darkest shade
       }, // Accent colors, used mainly for star color, heading and buttons
       orange: {
-        50: "#faf5e4", // Lightest shade
+        50: "#faf5e4",  // Lightest shade
         100: "#f5ebc8",
         200: "#f0e1ac",
         300: "#ebd790",
         400: "#e6cd74",
-        500: "#d4af37", // Main color
-        600: "#b9972f",
-        700: "#9e7f27",
-        800: "#83671f",
-        950: "#68500f", // Darkest shade
-      },
-      orange1: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fb713b",
-        400: "#fa5a15",
-        500: "#e14d0b",
-        600: "#ea580c",
+        500: "#d7b286", // Main color
+        600: "#c19a6e",
+        700: "#a8835a",
+        800: "#8f6c46",
+        900: "#765533",
+        950: "#5d3e20"  // Darkest shade
       }, // Primary colors, used mainly for links, buttons and svg icons
       red: colors.red, // Used for bookmark icon
       zinc: colors.zinc, // Used mainly for box-shadow
