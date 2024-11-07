51e7c5b8095df6423cc90fff52a37ec6f0eb1ca3
Nicolas Morales <jnicolasmoraless@hotmail.com>
2024-11-07 17:59:28 -0500

    update the Contact section
    I have added the location on the footer section.

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -14,6 +14,7 @@ const footerLinks = [
     links: [
       { name: "contact@weddingincartagena.com", url: "mailto:contact@weddingincartagena.com" },
       { name: "+573226943874", url: "https://wa.me/573226943874?text=%20%C2%A1Cont%C3%A1ctanos%20hoy%20y%20comienza%20a%20planificar%20la%20celebraci%C3%B3n%20de%20tus%20sue%C3%B1os!" },
+      { name: "Cartagena, Colombia", url: "https://www.google.com/maps/place/Cartagena,+Cartagena+Province,+Bolivar/@10.4001961,-75.5497248,13z/data=!3m1!4b1!4m6!3m5!1s0x8ef625e7ae9d1351:0xb161392e033f26ca!8m2!3d10.3932277!4d-75.4832311!16zL20vMGg0NW4?entry=ttu&g_ep=EgoyMDI0MTEwNS4wIKXMDSoASAFQAw%3D%3D" },
     ],
   },
   {
