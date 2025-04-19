c338b4a4788c3f26d0a32f727a2da31d77381992
Nicolas Morales <jnicolasmoraless@hotmail.com>
2025-04-19 13:45:31 -0500

    i18n Update

---
+++ src/i18n/es.ts
@@ -0,0 +1,32 @@
+const es = {
+    about: {
+      title: "Sobre Nosotros",
+      subTitle: `¡Bienvenidos a nuestra <span class="text-yellow-500 dark:text-yellow-400">familia</span> en el mundo donde los sueños se hacen realidad!`,
+      tabs: [
+        {
+          heading: "Superando Expectativas",
+          content:
+            "Nos esforzamos por superar tus expectativas en cada paso, llevando tus ideas desde la concepción hasta la realización con estilo y un toque personal incomparable.",
+          svg: "tools",
+          alt: "Equipo pesado amarillo y negro en un campo de pasto marrón",
+        },
+        {
+          heading: "Pasión por la Excelencia",
+          content:
+            "En el corazón de nuestra empresa hay una pasión inquebrantable por la excelencia en la planificación de bodas y eventos.",
+          svg: "dashboard",
+          alt: "Una captura o representación gráfica del panel de control intuitivo",
+        },
+        {
+          heading: "Momentos Inolvidables",
+          content:
+            "En Le Mariage, nos enorgullece presentarnos como tu aliado de confianza para crear momentos inolvidables en Cartagena y más allá.",
+          svg: "house",
+          alt: "Estructura metálica gris cerca de una grúa durante el día",
+        },
+      ],
+    },
+  };
+  
+  export default es;
+  
