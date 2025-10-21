f223d1024fd574806fdae394aeafe4c6ef67a60e
Nicolas Morales <jnicolasmoraless@hotmail.com>
2025-10-21 17:44:16 -0500

    Updating files

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -12,8 +12,8 @@ const footerLinks = [
   {
     section: "Contact",
     links: [
-      { name: "contact@weddingincartagena.com", url: "mailto:contact@weddingincartagena.com" },
-      { name: "+573226943874", url: "https://wa.me/573226943874?text=%20%C2%A1Cont%C3%A1ctanos%20hoy%20y%20comienza%20a%20planificar%20la%20celebraci%C3%B3n%20de%20tus%20sue%C3%B1os!" },
+      { name: "lemariagecartagena@hotmail.com", url: "mailto:lemariagecartagena@hotmail.com"},
+      { name: "+573004128389", url: "https://wa.me/+573004128389?text=%20%C2%A1Cont%C3%A1ctanos%20hoy%20y%20comienza%20a%20planificar%20la%20celebraci%C3%B3n%20de%20tus%20sue%C3%B1os!" },
       { name: "Cartagena, Colombia", url: "https://www.google.com/maps/place/Cartagena,+Cartagena+Province,+Bolivar/data=!4m2!3m1!1s0x8ef625e7ae9d1351:0xb161392e033f26ca?sa=X&ved=1t:242&ictx=111" },
     ],
   },
